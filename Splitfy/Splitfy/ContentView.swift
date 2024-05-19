44d8bc539ba48e044a98208a21db94379a785620
Ankit180898 <karan180898@gmail.com>
2024-05-19 20:29:50 +0530

    first project

---
+++ Splitfy/Splitfy/ContentView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  Splitfy
+//
+//  Created by Ankit Kumar on 16/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage=20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+     var totalPerPerson:Double{
+        let totalPerson=Double(numberOfPeople + 2)
+        let tipSelection=Double(tipPercentage)
+        var totalAmount = (checkAmount + (checkAmount/100 * tipSelection))/totalPerson
+        return totalAmount
+    }
+
+    var body: some View {
+        NavigationStack {
+            Form{
+                Section{
+                    TextField("Amount",value: $checkAmount,format: .currency(code: Locale.current.currency?.identifier ?? "INR")).keyboardType(.decimalPad)
+                   
+                    
+                    
+                }
+                Section{
+                    Picker(
+                        "Number of people",selection: $numberOfPeople){
+                            ForEach(2..<100){
+                                Text("\($0) people")
+                            }
+                            
+                        }.pickerStyle(.automatic)
+                }
+                Section("How much do you want to tip?"){
+                    Picker(
+                        "Tip percentage",selection: $tipPercentage){
+                            ForEach(tipPercentages,id: \.self){
+                                Text($0,format: .percent)
+                            }
+                            
+                        }
+                        .pickerStyle(.segmented)
+                }
+                
+                Section{
+                    Text(totalPerPerson,format: .currency(code: "INR"))
+                }
+                
+                
+            }.navigationTitle("Splitfy")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Splitfy/Splitfy/SplitfyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitfyApp.swift
+//  Splitfy
+//
+//  Created by Ankit Kumar on 16/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SplitfyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
