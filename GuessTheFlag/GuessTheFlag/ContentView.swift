247a3f45de298c18c2ebb89dfad5eade22d000e9
Ankit180898 <karan180898@gmail.com>
2024-05-26 18:20:10 +0530

    Day 23

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,16 +8,103 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert=false;
     var body: some View {
-        VStack {
+        VStack(alignment:.leading) {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
         }
         .padding()
+        HStack{
+            Text("Ankit")
+            Text("Kumar")
+            
+        }
+        ZStack{
+            Text("hello Khushii")
+            Text("I'm Ankit")
+        }.padding(20)
+        VStack {
+            HStack{
+                Spacer()
+                Text("1")
+                Spacer()
+                Text("2")
+                Spacer()
+                Text("3")
+                Spacer()
+                
+            }
+            
+            HStack{
+                Spacer()
+                Text("1")
+                Spacer()
+                Text("2")
+                Spacer()
+                Text("3")
+                Spacer()
+                
+            }
+            HStack{
+                Spacer()
+                Text("1")
+                Spacer()
+                Text("2")
+                Spacer()
+                Text("3")
+                Spacer()
+                
+            }
+            
+        }
+        ZStack{
+            //this color is adaptive to system settings
+//            Color.primary
+//            Color.accentColor
+                /*.frame(width: 200,height: 200)*/ //add a frame of certan height/width
+            //rgb
+//            Color(red:1,green: 0.4,blue: 2)
+//                .frame(minWidth: 200,maxWidth: .infinity,maxHeight: 200)
+//            Color.red
+//            Text("Ankit")
+            VStack(spacing: 0){
+            
+                Color.red
+                Color.blue
+            }
+            Text("your content")
+//                .foregroundColor(.secondary)
+                .foregroundStyle(.secondary)
+                .padding(30)
+                .background(.ultraThinMaterial)
+                .cornerRadius(20)
+                
+        }.ignoresSafeArea()
+        LinearGradient(gradient: Gradient(colors: [.white.opacity(0.5), .black.opacity(0.8)]), startPoint: .top, endPoint: .bottom)
+        LinearGradient(gradient: Gradient(stops: [.init(color: .red, location: 0.45),
+                                                  .init(color: .white, location: 0.47)
+                                                 ]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/)
+        RadialGradient(gradient: Gradient(colors: [.black,.white]), center: .center, startRadius: 20, endRadius: 200)
+        
+        Button("Alert"){
+            showingAlert=true
+        }
+        .alert("Important message",isPresented: $showingAlert){
+            Button("Delete",role: .destructive){
+                
+            }
+            Button("Cancel",role: .cancel){
+                
+            }
+        }message: {
+            Text("please read this")
+        }
+        
+        }
     }
-}
 
 #Preview {
     ContentView()
--- GuessTheFlag/GuessTheFlag/HomeView.swift
+++ GuessTheFlag/GuessTheFlag/HomeView.swift
@@ -5,4 +5,100 @@
 //  Created by Ankit Kumar on 25/05/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct HomeView:View {
+    @State private var scoreTitle=""
+    @State private var showingScore=false
+    @State private var score=0
+    @State private var countries=["Estonia","France","Germany","Ireland"].shuffled()
+    @State private var correctAnswer=Int.random(in: 0...2)
+    var body: some View {
+        ZStack{
+            
+//            LinearGradient(colors: [.blue,.black.opacity(0.8)], startPoint: .top, endPoint: .bottom)
+//                .ignoresSafeArea()
+            RadialGradient(stops:[
+                .init(color: .blue, location: 0.3),
+                .init(color:.black,location: 0.3)
+            ]
+                           ,center: .top,startRadius: 200,endRadius: 700
+            )
+            .ignoresSafeArea()
+            VStack{
+                Spacer()
+                Text("Guess the Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundColor(.white)
+                VStack(spacing:15){
+                    
+                    
+                    VStack{
+                        
+                        Text("Tap the countries of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                            .foregroundColor(.black)
+                            .font(.largeTitle.weight(.semibold))
+                        
+                    }
+                    ForEach(0..<3){ number in
+                        Button{
+                            showTapped(number)
+                        }label: {
+                            Image(countries[number])
+                                .renderingMode(.original)
+                                .clipShape(Capsule())
+                                .overlay(
+                                    Capsule().stroke(Color.white, lineWidth: 3) // Adjust color and lineWidth as needed
+                                )
+                                .shadow(radius: 5)
+                            
+                            
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical,20)
+                .background(.ultraThinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20.0 ))
+                Spacer()
+                Spacer()
+                
+                Text("Score: \(score)")
+                    .foregroundColor(.white)
+                    .font(.largeTitle.weight(.semibold))
+                Spacer()
+            }
+            .padding()
+            
+            
+            
+        }
+        .alert(scoreTitle,isPresented: $showingScore){
+            Button("Continue",action: askQues)
+        }message: {
+            Text("Your score is \(score)")
+        }
+    }
+        func showTapped(_ number:Int){
+            if number==correctAnswer{
+                scoreTitle="Correct"
+                score += 1
+            }
+            else{
+                scoreTitle="Incorrect"
+            }
+        showingScore=true
+       
+    }
+    func askQues(){
+        countries.shuffle()
+        correctAnswer=Int.random(in:0...2)
+    }
+}
+
+#Preview {
+    HomeView()
+}
