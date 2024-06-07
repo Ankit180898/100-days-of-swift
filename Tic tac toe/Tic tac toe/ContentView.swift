66b1ef1620ab7a9f51b109ec11a200f0a2bc2f4a
Ankit180898 <karan180898@gmail.com>
2024-06-07 10:13:31 +0530

    Initial settup

--- Tic tac toe/Tic tac toe/ContentView.swift
+++ Tic tac toe/Tic tac toe/ContentView.swift
@@ -8,15 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var moves : [Move?] = Array(repeating: nil, count: 9)
+    @State private var isHuman=false;
+    let columns: [GridItem]=[GridItem(.flexible()),GridItem(.flexible()),GridItem(.flexible())]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader{
+            geometryReader in
+                VStack(){
+                
+                    Spacer()
+                    LazyVGrid(columns: columns, content: {
+                        ForEach(0..<9){
+                            number in
+                            ZStack(){
+                                Circle()
+                                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/.opacity(0.5))
+                                
+                                Image(systemName: moves[number]?.indicator ?? "")
+                                    .resizable()
+                                    .frame(width: 40,height: 40)
+                                    .foregroundColor(.white)
+                                   
+                                
+                                
+                            }
+                            .onTapGesture {
+                                moves[number]=Move(player: isHuman ? .human : .computer, boardIndex: number)
+                                //computer's turn
+                                isHuman.toggle()
+                                
+                            }
+                        }
+                    })
+                    Spacer()
+                }
+                .padding()
+            
+         
+           
+
+            
         }
-        .padding()
+        .background(.indigo.opacity(0.2))
+        
+        
     }
+}
+
+enum Player{
+    case human, computer
+}
+
+struct Move{
+    let player:Player
+    let boardIndex:Int
+    var indicator:String{
+        return player == .human ? "xmark" : "circle"
+    }
+    
 }
 
 #Preview {
